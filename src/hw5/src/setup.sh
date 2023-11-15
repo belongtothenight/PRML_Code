14d9f67496d5cd4b4e3ca5711354f7e774e554f0
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-11-15 22:14:42 +0800

    Update setup.sh

--- src/hw5/src/setup.sh
+++ src/hw5/src/setup.sh
@@ -6,9 +6,9 @@ cd "/home/lab353/Documents/GitHub/PRML_Code/src/hw5/frcnn_chicken_1"
 pip install --upgrade pip;
 pip install pandas matplotlib pillow "keras==2.3" opencv-python scikit-learn;
 apt-get update
-# apt-get install sudo;
-# sudo apt update;
 sudo apt install -y libgl1-mesa-glx;
+
+bash run.sh;
 
 python train_frcnn.py;
 python test_frcnn.py;
