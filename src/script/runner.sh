89e169365f8af44314b291916a25e55b3aa028ec
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-11-15 23:11:46 +0800

    adjust runner.sh

--- src/script/runner.sh
+++ src/script/runner.sh
@@ -21,26 +21,29 @@ then
     exit 1
 fi
 
+FORMAT_BOLD_BLUE="\n\033[1m\033[34m"
+FORMAT_RESET="\033[0m"
+
 cwd=$(pwd)
 cd ../build/
 
 if [[ $NO_CLEAN != 1 ]]; then
-    echo -e "Cleaning ..."
+    echo -e "${FORMAT_BOLD_BLUE}Cleaning ...${FORMAT_RESET}"
     rm -rf *
 fi
 
-echo -e "Generating Makefile ..."
+echo -e "${FORMAT_BOLD_BLUE}Generating Makefile ...${FORMAT_RESET}"
 cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../ -DBUILD_SHARED_LIBS=ON
 
-echo -e "Building ..."
+echo -e "${FORMAT_BOLD_BLUE}Building ...${FORMAT_RESET}"
 make
 
-echo -e "Installing ..."
+echo -e "${FORMAT_BOLD_BLUE}Installing ...${FORMAT_RESET}"
 make install
 
-echo -e "Running ..."
+echo -e "${FORMAT_BOLD_BLUE}Running ...${FORMAT_RESET}"
 cd ../bin/
 ./$PROJECT
 
-echo -e "Done."
+echo -e "${FORMAT_BOLD_BLUE}Done.${FORMAT_RESET}"
 cd $cwd
